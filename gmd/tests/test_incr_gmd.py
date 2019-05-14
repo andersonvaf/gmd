1875050a7067905cb49723d05cd9ecea94d91873
Florian Kalinke <flops.ka@gmail.com>
2019-05-14 09:24:01 +0200

    add test to verify the incremental sorting

--- gmd/tests/test_incr_gmd.py
+++ gmd/tests/test_incr_gmd.py
@@ -1,12 +1,26 @@
 import unittest
 import numpy as np
+import pandas as pd
 
-from gmd import IncSortedIndex
+from gmd import IncSortedIndex, GMD
 
 class TestIncGMD(unittest.TestCase):
     """
     Test the incremental GMD implementation
     """
+
+    def gen_data(self, length):
+        return pd.DataFrame({'x1': np.random.uniform(0,1,length), 
+                   'x2' : np.random.uniform(0,1,length), 
+                   'x3' : np.random.uniform(0,1,length),
+                   'x4' : np.random.uniform(0,1,length),
+                   'x5' : np.random.uniform(0,1,length),
+                   'x6' : np.random.uniform(0,1,length),
+                   'x7' : np.random.uniform(0,1,length),
+                   'x8' : np.random.uniform(0,1,length),
+                   'x9' : np.random.uniform(0,1,length),
+                   'x10' : np.random.uniform(0,1,length),
+                  })
 
     def setUp(self):
         unsorted = np.array([[0.1, 1.4, 2.7],                             
@@ -35,6 +49,21 @@ class TestIncGMD(unittest.TestCase):
         self.sorted_index.del_and_ins_sorted(e1)
         self.sorted_index.del_and_ins_sorted(e2)
         self.assertListEqual(self.sorted_index.sorted.tolist(), [[2, 1, 2], [0, 2, 1], [1, 0, 0]])
+
+    def test_inc_insert_is_equal_to_regular_sorting(self):
+        df = self.gen_data(1000)
+        window_size = 50
+        start_window = df[:window_size]
+
+        sorted_index = IncSortedIndex(start_window)
+        for i in range(window_size, len(df)):
+            sorted_index.del_and_ins_sorted(df.iloc[i])
+
+        # the sorted_index should now equal the data structure we obtain by just
+        # sorting the last 100 elements
+
+        reference = GMD().create_sorted_index(df[-window_size:])
+        self.assertListEqual(sorted_index.sorted.tolist(), reference.tolist())
 
 if __name__ == '__main__':
     unittest.main()
