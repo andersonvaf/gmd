86bdb325fd033fdd8f5596607a41429da7bab6d3
Florian Kalinke <flops.ka@gmail.com>
2019-05-14 08:38:30 +0200

    fix tests for incremental sorting

--- gmd/__init__.py
+++ gmd/__init__.py
@@ -1,5 +1,6 @@
 from .gmd import GMD
+from .incsortedindex import IncSortedIndex
 
 from ._version import __version__
 
-__all__ = ['GMD', '__version__']
+__all__ = ['GMD', '__version__', 'IncSortedIndex']
